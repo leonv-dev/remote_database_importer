2bd045b3ee78bac2acf4a3590c4dca96ceb473a6
Leon <nonick@nonick.ch>
2022-10-22 17:03:19 +0200

    first commit

---
+++ lib/remote_database_importer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "remote_database_importer/version"
+
+module RemoteDatabaseImporter
+  class Error < StandardError; end
+  # Your code goes here...
+end
