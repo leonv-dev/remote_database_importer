48c40ba5923bf178f7916fa0c8098c2f293bb1f8
Leon <nonick@nonick.ch>
2022-10-22 18:28:59 +0200

    refactoring

--- lib/remote_database_importer/operation.rb
+++ lib/remote_database_importer/operation.rb
@@ -4,6 +4,8 @@ module RemoteDatabaseImporter
     require "ruby-progressbar"
     require "pry"
 
+    LOG_FILE = "tmp/remote_database_importer.log"
+
     def initialize
       config_handler = RemoteDatabaseImporter::Config.new
       @config = config_handler.read_or_create_configfile
@@ -42,7 +44,7 @@ module RemoteDatabaseImporter
       progressbar = ProgressBar.create(title: "Import remote DB", total: tasks.length, format: "%t %p%% %B %a")
       tasks.each do |task|
         was_good = system(task)
-        return "Can't continue, following task failed: #{task}" unless was_good
+        return "Can't continue, following task failed: #{task} - checkout the logfile: #{LOG_FILE}" unless was_good
         progressbar.increment
       end
     end
@@ -50,27 +52,32 @@ module RemoteDatabaseImporter
     private
 
     def terminate_current_db_sessions
-      "psql -d #{@config.fetch("local_db")} -c 'SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();' > remote_database_importer.log"
+      "psql -d #{@config.fetch("local_db")} -c 'SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();' > #{LOG_FILE}"
     end
 
     def dump_remote_db(env)
-      "ssh #{env["ssh_connection"]["user"]}@#{env["ssh_connection"]["host"]} 'pg_dump -Fc -U #{env["database"]["user"]} -d #{env["database"]["name"]} -h localhost -C' > #{env["database"]["name"]}.dump"
+      ssh_user = env["ssh_connection"]["user"]
+      ssh_host = env["ssh_connection"]["host"]
+      db_user = env["database"]["user"]
+      db_name = env["database"]["name"]
+      "ssh #{ssh_user}@#{ssh_host} 'pg_dump -Fc -U #{db_user} -d #{db_name} -h localhost -C' > #{db_name}.dump"
     end
 
     def drop_and_create_local_db
-      "rails db:environment:set RAILS_ENV=development; rake db:drop db:create > remote_database_importer.log"
+      "rails db:environment:set RAILS_ENV=development; rake db:drop db:create > #{LOG_FILE}"
     end
 
     def restore_db(env)
-      "pg_restore --jobs 8 --no-privileges --no-owner --dbname #{@config.fetch("local_db")} #{env["database"]["name"]}.dump"
+      db_name = env["database"]["name"]
+      "pg_restore --jobs 8 --no-privileges --no-owner --dbname #{@config.fetch("local_db")} #{db_name}.dump"
     end
 
     def run_migrations
-      "rake db:migrate > remote_database_importer.log"
+      "rake db:migrate > #{LOG_FILE}"
     end
 
     def clear_logfile
-      "rm remote_database_importer.log"
+      "rm #{LOG_FILE}"
     end
   end
 end
